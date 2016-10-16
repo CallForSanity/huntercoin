9263f1e7fca125b02443ab8ef7e6093d5ba81286
Patrick Charrier <patrick.charrier@gmail.com>
2016-10-16 11:52:34 +0200

    added Vagrant and multichain files

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,10 @@ wget http://www.multichain.com/download/multichain-1.0-alpha-9.tar.gz
 tar -xvzf multichain-1.0-alpha-9.tar.gz
 cd multichain-1.0-alpha-9
 
-./multichain-util create NoFrontieres
-./multichaind NoFrontieres -daemon
+#./multichain-util create NoFrontieres
+mkdir -p .multichain/NoFrontieres
+cp /vagrant/bitcoin-protocol-params.dat ~/.multichain/NoFrontieres/params.dat
+#./multichaind NoFrontieres -daemon
 
 # http://www.multichain.com/developers/peer-handshaking/#bitcoin
 # http://www.multichain.com/developers/creating-connecting/#bitcoin-style
